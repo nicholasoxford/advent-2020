af3348b34c917bd26b6a43242cc7498a0e2f6f5b
Nicholas Oxford <51415676+nicholasoxford@users.noreply.github.com>
2020-12-01 23:09:21 -0800

    Update password.js

--- 2/password.js
+++ 2/password.js
@@ -5,28 +5,21 @@ var array = fs.readFileSync('input.txt').toString().split("\n");
 var length = array.length - 1
 var i = 0;
 var v = 0;
-
 while (i < length)
 {
     var infoAndPassword = array[i].split(": ")
     var minMax = infoAndPassword[0].split("-")
     var letterCheck = minMax[1].split(" ");
-    var infoCheck = {
-        minNumber: Number(minMax[0]),
-        maxNumber: Number(letterCheck[0]),
-        specialChar: letterCheck[1],
-        password: infoAndPassword[1],
-    }
-    var num1 = infoCheck.minNumber - 1;
-    var num2 = infoCheck.maxNumber - 1
+    var num1 = Number(minMax[0]) - 1;
+    var num2 = Number(letterCheck[0]) - 1
 
-    var textArray = infoCheck.password.split("")
-    if (textArray[num1] && textArray[num1].toString() === infoCheck.specialChar.toString())
-        if (textArray[num2] && textArray[num2].toString() == infoCheck.specialChar.toString()) 
+    var textArray = infoAndPassword[1].split("")
+    if (textArray[num1] && textArray[num1].toString() === letterCheck[1].toString())
+        if (textArray[num2] && textArray[num2].toString() == letterCheck[1].toString()) 
             nextTick
         else 
             v++
-    else if (textArray[num2] && textArray[num2].toString() === infoCheck.specialChar.toString()) 
+    else if (textArray[num2] && textArray[num2].toString() === letterCheck[1].toString()) 
         v++
     else
         nextTick
