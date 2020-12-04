73b020c729b774116880f8adf3c4fef01d895e09
Nicholas Oxford <51415676+nicholasoxford@users.noreply.github.com>
2020-12-03 17:26:11 -0800

    Update three.js

--- 3/three.js
+++ 3/three.js
@@ -10,20 +10,18 @@ var testObj = [
 testObj.forEach((tuple) => {
   tuple[2] = checkTree(tuple);
 });
-console.log(testObj[0][2]*testObj[1][2]*testObj[2][2]*testObj[3][2]*testObj[4][2]);
-    
-function checkTree( cords) {
-  var x = cords[0];
-  var y = cords[1];
+console.log(testObj[0][2] * testObj[1][2] * testObj[2][2] * testObj[3][2] * testObj[4][2]);
+
+function checkTree(cords) {
   var tempTree = 0;
   var i = 0;
   var t = [0, 0];
   while (i < array.length - 1) {
     var tempString = array[t[1]];
-      if (tempString) tempString = tempString.repeat(Math.floor(t[0] / 10) + 1);
-      if (tempString && tempString[t[0]] == "#") tempTree = tempTree + 1;
-    t[0] += x;
-    t[1] += y;
+    if (tempString) tempString = tempString.repeat(Math.floor(t[0] / 10) + 1);
+    if (tempString && tempString[t[0]] == "#") tempTree = tempTree + 1;
+    t[0] += cords[0];
+    t[1] += cords[1];
     i++;
   }
   return tempTree;
